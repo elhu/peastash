856d881268453cf6ec85dd2dcf7371a169316ed4
Vincent Boisard <vincent.boisard@dimelo.com>
2014-07-01 15:49:41 +0200

    fix: maintain compat with logstasher

--- lib/peastash/rails_ext/railtie.rb
+++ lib/peastash/rails_ext/railtie.rb
@@ -8,12 +8,14 @@ class Peastash
     initializer 'peastash.configure' do |app|
       if app.config.peastash[:enabled]
         Peastash.with_instance.configure!(app.config.peastash)
-        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |name, started, finished, unique_id, data|
+        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |name, started, finished, unique_id, payload|
           # Handle parameters and sanitize if need be
+          payload = data.reject(:db_runtime, :view_runtime)
+          payload.merge!(db: data[:db_runtime], view: data[:view_runtime])
           if Peastash.with_instance.configuration[:log_parameters]
-            data[:params].reject! { |k, _| ActionController::LogSubscriber::INTERNAL_PARAMS.include?(k) }
+            payload[:params].reject { |k, _| ActionController::LogSubscriber::INTERNAL_PARAMS.include?(k) }
           else
-            data.delete(:params)
+            payload.delete(:params)
           end
           # Preserve explicitely set data
           Peastash.with_instance.store.merge!(data) { |key, old_val, new_val| old_val }
