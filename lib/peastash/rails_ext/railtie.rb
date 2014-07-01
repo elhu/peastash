5d4b7ed06336ea5501a2804c528c0485db55f530
Vincent Boisard <vincent.boisard@dimelo.com>
2014-07-01 16:17:14 +0200

    fix: failing specs

--- lib/peastash/rails_ext/railtie.rb
+++ lib/peastash/rails_ext/railtie.rb
@@ -8,9 +8,10 @@ class Peastash
     initializer 'peastash.configure' do |app|
       if app.config.peastash[:enabled]
         Peastash.with_instance.configure!(app.config.peastash)
-        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |name, started, finished, unique_id, payload|
+        ActiveSupport::Notifications.subscribe('process_action.action_controller') do |name, started, finished, unique_id, data|
           # Handle parameters and sanitize if need be
-          payload = data.reject(:db_runtime, :view_runtime)
+          to_reject = [:db_runtime, :view_runtime]
+          payload = data.reject { |key, _| to_reject.include?(key) }
           payload.merge!(db: data[:db_runtime], view: data[:view_runtime])
           if Peastash.with_instance.configuration[:log_parameters]
             payload[:params].reject { |k, _| ActionController::LogSubscriber::INTERNAL_PARAMS.include?(k) }
@@ -18,7 +19,7 @@ class Peastash
             payload.delete(:params)
           end
           # Preserve explicitely set data
-          Peastash.with_instance.store.merge!(data) { |key, old_val, new_val| old_val }
+          Peastash.with_instance.store.merge!(payload) { |key, old_val, new_val| old_val }
         end
         app.config.middleware.use Peastash::Middleware
       end
