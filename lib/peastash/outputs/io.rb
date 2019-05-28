3da2ade262649bd77d19cfca8411cefe79837c71
Yoann Lecuyer <yoann.lecuyer@dimelo.com>
2019-05-28 12:17:54 +0200

    Better: Use realpath for logdevice. (#17)
    * Better: Use realpath for logdevice.
    
    * Keep working with IO
    
    * Add doc + cleaning

--- lib/peastash/outputs/io.rb
+++ lib/peastash/outputs/io.rb
@@ -10,6 +10,16 @@ class Peastash
       end
 
       def initialize(file, *args)
+        if file.is_a?(String)
+          # Rewrite symlink path to realpath for instance
+          # /home/app/releases/20190528155050/log/logstash.log -> /home/app/shared/log/logstash.log
+          # if the symlinked folder gets deleted on further releases, the log rotation will fail with
+          # a long wait ending with : log rotation inter-process lock failed.
+          # realpath is called without the filename because it expects the full path to exists
+          dir = File.realpath(File.dirname(file))
+          name = File.basename(file)
+          file = "#{dir}/#{name}"
+        end
         @device = ::Peastash::LogDevice.new(file, *args)
       end
 
