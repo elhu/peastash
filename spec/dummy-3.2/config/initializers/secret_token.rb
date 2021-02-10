d33ddc747fe8a9546bb6845b14fec08996de904e
Adrien Rey-Jarthon <adrien.jarthon@ringcentral.com>
2021-02-10 19:55:04 +0100

    Fix Railties specs + add Rails 4.2, 6.0 and 6.1

--- spec/dummy-3.2/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy32::Application.config.secret_token = 'b1ba4c565eb42c7c4efd91b5f7d4c7ad23cb7531a2119363a7a4f90e0d5d2379e5c39302a571e01cf132ce47e3fb20fe4e3a135fe632058de053d11f7401e11a'
